cc67a6d48aa9219f8753aa394a39bf882e3b5fa5
Luv <sabasaltemail55@gmail.com>
2025-06-27 15:10:35 +0400

    Update flip.lua

--- flip.lua
+++ flip.lua
@@ -1,85 +1,57 @@
-wait(5)
-
---[[ Info ]]--
-
-local ver = "2.00"
-local scriptname = "feFlip"
-
-
---[[ Keybinds ]]--
-
 local FrontflipKey = Enum.KeyCode.Z
 local BackflipKey = Enum.KeyCode.X
 local AirjumpKey = Enum.KeyCode.C
-
-
---[[ Dependencies ]]--
-
 local ca = game:GetService("ContextActionService")
 local zeezy = game:GetService("Players").LocalPlayer
 local h = 0.0174533
-local antigrav
 
-
---[[ Functions ]]--
-
-function zeezyFrontflip(act,inp,obj)
+function zeezyFrontflip(act, inp, obj)
 	if inp == Enum.UserInputState.Begin then
 		zeezy.Character.Humanoid:ChangeState("Jumping")
-		wait()
+		task.wait()
 		zeezy.Character.Humanoid.Sit = true
-		for i = 1,360 do 
-			delay(i/720,function()
-			zeezy.Character.Humanoid.Sit = true
-				zeezy.Character.HumanoidRootPart.CFrame = zeezy.Character.HumanoidRootPart.CFrame * CFrame.Angles(-h,0,0)
+		for i = 1, 360 do 
+			task.delay(i/720, function()
+				if zeezy.Character and zeezy.Character:FindFirstChild("HumanoidRootPart") then
+					zeezy.Character.Humanoid.Sit = true
+					zeezy.Character.HumanoidRootPart.CFrame *= CFrame.Angles(-h, 0, 0)
+				end
 			end)
 		end
-		wait(0.55)
+		task.wait(0.55)
 		zeezy.Character.Humanoid.Sit = false
 	end
 end
 
-function zeezyBackflip(act,inp,obj)
+function zeezyBackflip(act, inp, obj)
 	if inp == Enum.UserInputState.Begin then
 		zeezy.Character.Humanoid:ChangeState("Jumping")
-		wait()
+		task.wait()
 		zeezy.Character.Humanoid.Sit = true
-		for i = 1,360 do
-			delay(i/720,function()
-			zeezy.Character.Humanoid.Sit = true
-				zeezy.Character.HumanoidRootPart.CFrame = zeezy.Character.HumanoidRootPart.CFrame * CFrame.Angles(h,0,0)
+		for i = 1, 360 do
+			task.delay(i/720, function()
+				if zeezy.Character and zeezy.Character:FindFirstChild("HumanoidRootPart") then
+					zeezy.Character.Humanoid.Sit = true
+					zeezy.Character.HumanoidRootPart.CFrame *= CFrame.Angles(h, 0, 0)
+				end
 			end)
 		end
-		wait(0.55)
+		task.wait(0.55)
 		zeezy.Character.Humanoid.Sit = false
 	end
 end
 
-function zeezyAirjump(act,inp,obj)
+function zeezyAirjump(act, inp, obj)
 	if inp == Enum.UserInputState.Begin then
-		zeezy.Character:FindFirstChildOfClass'Humanoid':ChangeState("Seated")
-		wait()
-		zeezy.Character:FindFirstChildOfClass'Humanoid':ChangeState("Jumping")	
+		local hum = zeezy.Character:FindFirstChildOfClass("Humanoid")
+		if hum then
+			hum:ChangeState("Seated")
+			task.wait()
+			hum:ChangeState("Jumping")
+		end
 	end
 end
 
-
---[[ Binds ]]--
-
-ca:BindAction("zeezyFrontflip",zeezyFrontflip,false,FrontflipKey)
-ca:BindAction("zeezyBackflip",zeezyBackflip,false,BackflipKey)
-ca:BindAction("zeezyAirjump",zeezyAirjump,false,AirjumpKey)
-
---[[ Load Message ]]--
-
-print(scriptname .. " " .. ver .. " loaded successfully")
-print("made by Zeezy#7203")
-
-local notifSound = Instance.new("Sound",workspace)
-notifSound.PlaybackSpeed = 1.5
-notifSound.Volume = 0.15
-notifSound.SoundId = "rbxassetid://170765130"
-notifSound.PlayOnRemove = true
-notifSound:Destroy()
-game.StarterGui:SetCore("SendNotification", {Title = "feFlip", Text = "feFlip loaded successfully!", Icon = "rbxassetid://505845268", Duration = 5, Button1 = "Okay"})
-
+ca:BindAction("zeezyFrontflip", zeezyFrontflip, false, FrontflipKey)
+ca:BindAction("zeezyBackflip", zeezyBackflip, false, BackflipKey)
+ca:BindAction("zeezyAirjump", zeezyAirjump, false, AirjumpKey)
